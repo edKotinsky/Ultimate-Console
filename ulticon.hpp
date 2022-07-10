2b8a69285f71e504d9185a6a3abb40ddad8d4a87
edKotinsky <edkotinsky@gmail.com>
2022-07-10 17:25:33 +0600

    Documentation fix

--- ulticon.hpp
+++ ulticon.hpp
@@ -2,9 +2,7 @@
     @file ulticon.hpp
     @brief Main ulticon header file
     @authors _edKotinsky
-    @date 23.06.22
-    @warning This is my first-test-hobby project
-        and it is not intended to practical usage  
+    @date 23.06.22  
 
     @mainpage Ultimate Console mainpage
     This is my first-test-hobby project
